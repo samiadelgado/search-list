dae09e0ca27947541ef5234f64db7da5b9c77269
Sami A.Delgado <samiadelgado@gmail.com>
2017-11-29 17:03:24 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,36 +1,101 @@
-var app = angular.module('universidadApp', []);
+(function(){
 
-app.controller('profesorCtrl',function ($scope) {
+var app = angular.module('universidadApp',[ ]);
 
-	$scope.profesor = profesorData;
-	$scope.editando={};
-	$scope.mostrarCaja = false;
 
-	$scope.EditarProfesor = function(){
 
-		angular.copy($scope.profesor, $scope.editando);
-		$scope.mostrarCaja = true;
+app.controller('listadoCtrl', ['$scope', function($scope){
+ 
 
-	}
+$scope.personas=[
+      {
+        "id": 0,
+        "sexo": "hombre",
+        "nombre": "Kari Carr",
+        "avatar": "kari.jpg",
+        "telefono": "(826) 453-3497",
+        "celular": "(801) 9175-8136"
+      },
+      {
+        "id": 1,
+        "sexo": "mujer",
+        "nombre": "Tameka Gamble",
+        "avatar": "tameka.jpg",
+        "telefono": "(824) 438-2499",
+        "celular": "(801) 8595-8337"
+      },
+      {
+        "id": 2,
+        "sexo": "mujer",
+        "nombre": "Charity Austin",
+        "avatar": "charity.jpg",
+        "telefono": "(817) 512-2258",
+        "celular": "(801) 9375-3830"
+      },
+      {
+        "id": 3,
+        "sexo": "mujer",
+        "nombre": "Slater Hunt",
+        "avatar": "slater.jpg",
+        "telefono": "(842) 413-3023",
+        "celular": "(801) 9555-1729"
+      },
+      {
+        "id": 4,
+        "sexo": "mujer",
+        "nombre": "Chen Hanson",
+        "avatar": "chen.jpg",
+        "telefono": "(966) 520-2696",
+        "celular": "(801) 9324-4423"
+      },
+      {
+        "id": 5,
+        "sexo": "hombre",
+        "nombre": "Obrien Davis",
+        "avatar": "obrien.jpg",
+        "telefono": "(996) 595-3896",
+        "celular": "(801) 8195-2732"
+      },
+      {
+        "id": 6,
+        "sexo": "hombre",
+        "nombre": "Le Haley",
+        "avatar": "le.jpg",
+        "telefono": "(967) 527-3286",
+        "celular": "(801) 8074-5225"
+      },
+      {
+        "id": 7,
+        "sexo": "hombre",
+        "nombre": "Lester Carney",
+        "avatar": "lester.jpg",
+        "telefono": "(994) 465-3542",
+        "celular": "(801) 9044-7522"
+      },
+      {
+        "id": 8,
+        "sexo": "hombre",
+        "nombre": "Rosario Perry",
+        "avatar": "rosario.jpg",
+        "telefono": "(848) 499-2977",
+        "celular": "(801) 8495-0625"
+      },
+      {
+        "id": 9,
+        "sexo": "mujer",
+        "nombre": "Marilyn Huber",
+        "avatar": "marilyn.jpg",
+        "telefono": "(982) 580-3235",
+        "celular": "(801) 8184-2624"
+      }
+    ];
 
-	$scope.GuardarCambios = function(){
 
-		angular.copy($scope.editando, $scope.profesor);
-		$scope.mostrarCaja = false;
 
-	}
+}]);
 
-	$scope.CancelarCambios = function(){
 
-		$scope.editando = {};
-		$scope.mostrarCaja = false;
 
-	}
-});
 
-var profesorData = {
-	nombre: "Juan Carlos Pineda",
-	bio: "Saludos estudiante, mi nombre es Juan Carlos,encantado de conocerte",
-	edad: 47,
-	foto: "img/juancarlos.jpg"
-}
+
+})();
